b6a243b7e19ca7fae811f964f139de4369a88fef
Melo <845584@cognizant.com>
2020-02-11 14:12:27 -0300

    primeiro commit

---
+++ dynamodb/src/main/java/br/com/dynamodb/DynamoDBApplication.java
@@ -0,0 +1,13 @@
+
+package br.com.dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDBApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DynamoDBApplication.class, args);
+	}
+}
